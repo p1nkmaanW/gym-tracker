4c4b48344d0bef09c8a489079e591d94d5bd09a1
GymLog User <adem.bouzazi@tha.de>
2025-11-25 23:32:29 -0300

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-// We use 'any' here to stop TypeScript from complaining about the new feature
-const nextConfig: any = {
-  experimental: {
-    allowedDevOrigins: ["localhost:3000", "192.168.1.108:3000"],
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
